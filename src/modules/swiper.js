79cad075444876114820b7685fccf0ef011765a5
Nikita Korotkevich <kne@webernetic.by>
2023-02-10 17:54:10 +0300

    markup updates

--- src/modules/swiper.js
+++ src/modules/swiper.js
@@ -3,8 +3,15 @@ import "swiper/css/bundle";
 
 const swiper = new Swiper(".swiper.main-swiper", {
   direction: "horizontal",
-  loop: true,
-  slidesPerView: 2,
+  loop: false,
+  slidesPerView: 1,
+  spaceBetween: 0,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 25,
+    }
+  },
   navigation: {
     nextEl: ".main-swiper .swiper-button-next",
     prevEl: ".main-swiper .swiper-button-prev",
