47fee5e34b18e8a3c2003871e6938c1b8534fddd
Nikita Korotkevich <kne@webernetic.by>
2023-02-13 17:52:42 +0300

    front fixes

--- src/index.js
+++ src/index.js
@@ -2,3 +2,18 @@ import "@babel/polyfill";
 import "./index.html";
 import "./index.scss";
 import "./modules/swipers";
+
+const tabs = document.querySelectorAll(".main__schedule-options a");
+
+if (tabs.length) {
+	tabs.forEach((tab) => {
+		tab.addEventListener("click", (e) => {
+			e.preventDefault();
+			let active = document.querySelector(".active");
+			if (active) {
+				active.classList.remove("active");
+			}
+			e.target.classList.add("active");
+		})
+	})
+}
