d896fb0f625dce099d162f756e432511c5e0b80d
Nikita Korotkevich <kne@webernetic.by>
2023-02-20 17:51:34 +0300

    front fixes

--- src/modules/adaptive.js
+++ src/modules/adaptive.js
@@ -31,4 +31,64 @@ document.addEventListener("DOMContentLoaded", () => {
 			})
 		}
 	}
+
+	const reviewBtn = document.querySelector(".send-review.btn");
+	const reviewPopup = document.querySelector(".review-popup");
+
+	if (reviewBtn && reviewPopup) {
+		reviewBtn.addEventListener("click", (e) => {
+			e.preventDefault();
+			reviewPopup.classList.add("active");
+		})
+
+		let closeBtn = document.querySelector(".close-btn");
+		if (closeBtn) {
+			closeBtn.addEventListener("click", (e) => {
+				e.preventDefault();
+				if (reviewPopup.classList.contains("active")) {
+					reviewPopup.classList.remove("active");
+				}
+			})
+		}
+	}
+
+	const schBtn = document.querySelector(".header .header__right .header-btn");
+	const schPopup = document.querySelector(".schedule-popup");
+	
+	if (schPopup && schBtn) {
+		schBtn.addEventListener("click", (e) => {
+			e.preventDefault();
+			schPopup.classList.add("active");
+		})
+
+		let closeBtn = document.querySelector(".close-btn");
+		if (closeBtn) {
+			closeBtn.addEventListener("click", (e) => {
+				e.preventDefault();
+				if (schPopup.classList.contains("active")) {
+					schPopup.classList.remove("active");
+				}
+			})
+		}
+	}
+
+	const callbackBtn = document.querySelector(".phone-icon");
+	const callbackPopup = document.querySelector(".callback-popup");
+	
+	if (callbackBtn && callbackPopup) {
+		callbackBtn.addEventListener("click", (e) => {
+			e.preventDefault();
+			callbackPopup.classList.add("active");
+		})
+
+		let closeBtn = document.querySelector(".close-btn");
+		if (closeBtn) {
+			closeBtn.addEventListener("click", (e) => {
+				e.preventDefault();
+				if (callbackPopup.classList.contains("active")) {
+					callbackPopup.classList.remove("active");
+				}
+			})
+		}
+	}
 })
